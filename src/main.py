a8128c683c0775d39cf2e33a3511fd6b9f650a38
yumi2344 <42453373@qq.com>
2025-02-23 20:35:56 +0800

    添加贪吃蛇游戏

---
+++ src/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Welcome to Yumi Project!")
+    print("This is a sample Python application.")
+
+if __name__ == "__main__":
+    main() 
---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+def test_sample():
+    """
+    A sample test function
+    """
+    assert True, "This test should always pass"
+
+if __name__ == "__main__":
+    test_sample() 
